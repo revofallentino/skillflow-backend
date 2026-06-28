9dd6dd6b6505084f3af260e8f92a63fec5673061
Revo Fallentino <revofallentino@gmail.com>
2026-06-28 17:28:52 +0700

    fix: add port and ssl support for railway mysql

--- src/config/database.js
+++ src/config/database.js
@@ -7,8 +7,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
